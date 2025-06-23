e5c97431dd18e33b497214a8fcb55a102d6c89fe
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2025-06-23 20:06:30 +0200

    basic project setup

---
+++ lib/userverse_dart_client.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/userverse_dart_client_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
